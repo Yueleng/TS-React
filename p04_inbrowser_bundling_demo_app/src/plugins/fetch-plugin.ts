704ce88ca989934218ee37fe8ae5e6ed4810f431
Yueleng <wangyueleng@gmail.com>
2022-09-17 14:58:46 -0400

    split onLoad logic

--- p04_inbrowser_bundling_demo_app/src/plugins/fetch-plugin.ts
+++ p04_inbrowser_bundling_demo_app/src/plugins/fetch-plugin.ts
@@ -1,4 +1,4 @@
-import { PluginBuild, OnLoadResult } from "esbuild-wasm";
+import { PluginBuild, OnLoadResult, OnLoadArgs } from "esbuild-wasm";
 import axios from "axios";
 import localForage from "localforage";
 
@@ -18,15 +18,14 @@ export const fetchPlugin = (input: string) => {
   return {
     name: "fetch-plugin",
     setup(build: PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args);
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: input,
-          };
-        }
+      build.onLoad({ filter: /(^index\.js$)/ }, (args: OnLoadArgs) => {
+        return {
+          loader: "jsx",
+          contents: input,
+        };
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: OnLoadArgs) => {
         // Check to see if we have already fetched this file
         // and if it is in the cache
         const cachedResult = await fileCache.getItem<OnLoadResult>(args.path);
@@ -43,23 +42,50 @@ export const fetchPlugin = (input: string) => {
 
         console.log(request);
 
-        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
         const escaped = data
           .replace(/\n/g, "")
           .replace(/'/g, "\\'")
           .replace(/"/g, '\\"');
-        const contents =
-          fileType === "css"
-            ? `
+        const contents = `
             const style = document.createElement('style');
             style.innerText = '${escaped}';
             document.head.appendChild(style);
-          `
-            : data;
+          `;
 
         const result: OnLoadResult = {
           loader: "jsx",
-          contents: contents,
+          contents,
+          // resovleDir: "nested-test-pkg@1.0.0/src"
+          resolveDir: new URL("./", request.responseURL).pathname, // ship to onResolve as containing folder
+        };
+
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+
+        // Check to see if we have already fetched this file
+        // and if it is in the cache
+        const cachedResult = await fileCache.getItem<OnLoadResult>(args.path);
+
+        // if it is, return it immediately.
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        /**
+         * request gives info whether the request is redirected to somewhere
+         */
+        const { data, request } = await axios.get(args.path);
+
+        console.log(request);
+        const result: OnLoadResult = {
+          loader: "jsx",
+          contents: data,
           // resovleDir: "nested-test-pkg@1.0.0/src"
           resolveDir: new URL("./", request.responseURL).pathname, // ship to onResolve as containing folder
         };
